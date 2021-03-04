5c36a4aa2990c2552dc02455302c8e4083ca35bf
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-04 03:37:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if !position_taken?(board, index) && (index).between?(0,8)
     return true
-  elsif
+  elsif position_taken?(board, index) == true
     return false
   end
 end
