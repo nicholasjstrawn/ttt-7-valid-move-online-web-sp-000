188e9563635e810f3bd1a99232dd0305cc2fbd3b
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-04 03:29:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " or board[index] == "" or board[index] == nil
     return false
-  elsif board[index] == "X" or board[index] == "O"
+  elsif
     return true
   end
 end
